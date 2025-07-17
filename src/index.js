934be0051530f6bd8ef3cadbe51bfaa2a221c883
atif7luqman <atifluqman28@gmail.com>
2025-07-17 11:00:43 +0500

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import dotenv from 'dotenv';
-import express from 'express';
 import connectDB from './db/db.js';
+import { app } from './app.js';
 
 dotenv.config();
-
-const app = express();
 
 connectDB()
 .then(() => {
